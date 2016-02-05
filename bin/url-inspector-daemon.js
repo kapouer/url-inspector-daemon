5159b018df207b9241fa369beecd1227873effb8
Jérémy Lal <kapouer@melix.org>
2016-02-05 13:00:23 +0100

    Thumbnail support, cache them

--- bin/url-inspector-daemon.js
+++ bin/url-inspector-daemon.js
@@ -2,7 +2,11 @@
 
 var express = require('express');
 var app = express();
+var crypto = require('crypto');
 var inspector = require('url-inspector');
+var fs = require('fs');
+var mime = require('mime');
+var dataUri = require('strong-data-uri');
 var sharpie = require('sharpie')({
 	rs: "w:320,h:240,max",
 	bg: 'white',
@@ -10,14 +14,32 @@ var sharpie = require('sharpie')({
 	flatten: true
 });
 
+function hash(buf) {
+	return crypto.createHash('sha1').update(buf).digest("base64").replace(/[+=?]/g, '');
+}
+
+var rootDir = __dirname + '/..';
+
+app.use('/cache', express.static(rootDir + '/cache'));
+
 app.get('/images', sharpie);
 
 app.get('/inspector', function(req, res, next) {
 	inspector(req.query.url, function(err, data) {
 		if (err) return next(err);
-		if (data.thumbnail) {
-			if (data.thumbnail.startsWith('data:') == false) {
-				data.thumbnail = '/images?url=' + encodeURIComponent(data.thumbnail);
+		var thumb = data.thumbnail;
+		if (thumb) {
+			if (thumb.startsWith('data:') == false) {
+				data.thumbnail = '/images?url=' + encodeURIComponent(thumb);
+			} else {
+				var buf = dataUri.decode(thumb);
+				var filePath = '/cache/' + hash(buf) + '.' + mime.extension(buf.mimetype);
+				fs.writeFile(rootDir + filePath, buf, function(err) {
+					if (err) console.error(err);
+					else data.thumbnail = '/images?url=' + encodeURIComponent(filePath);
+					res.send(data);
+				});
+				return;
 			}
 		} else if (data.type == "image") {
 			data.thumbnail = '/images?url=' + encodeURIComponent(data.url);
